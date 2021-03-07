a5262805d5029eff95d4415f4bacc6f6947da7ad
jejaksaid <said@fastlearningcenter.com>
2021-03-08 00:25:46 +0700

    gatsby-config.js [module.export => module.exports]

--- gatsby-course/.cache/api-runner-ssr.js
+++ gatsby-course/.cache/api-runner-ssr.js
@@ -1,4 +1,8 @@
-var plugins = []
+var plugins = [{
+      name: 'gatsby-plugin-mdx',
+      plugin: require('/home/said/gatsby/gatsby-course/node_modules/gatsby-plugin-mdx/gatsby-ssr'),
+      options: {"plugins":[],"extensions":[".mdx",".md"],"defaultLayouts":{},"gatsbyRemarkPlugins":[],"lessBabel":false,"remarkPlugins":[],"rehypePlugins":[],"mediaTypes":["text/markdown","text/x-markdown"],"root":"/home/said/gatsby/gatsby-course"},
+    }]
 // During bootstrap, we write requires at top of this file which looks like:
 // var plugins = [
 //   {
