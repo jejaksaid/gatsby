a63a8c3a2a693e731f4bd14d4df8de457e341bd9
jejaksaid <said@fastlearningcenter.com>
2021-04-17 15:13:28 +0700

    Navbar and Links

--- saidgatsby/src/pages/index.js
+++ saidgatsby/src/pages/index.js
@@ -1,5 +1,12 @@
 import React from "react"
+import Navbar from "../components/Navbar"
 
-export default function Home() {
-  return <div>Hello world!</div>
+function Home() {
+  return (
+    <section>
+      <Navbar />
+    </section>
+  )
 }
+
+export default Home
