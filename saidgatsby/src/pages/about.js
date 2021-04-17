e7d43b0f8e73c8f43f4d0a6666e2b33c1e2d6b04
jejaksaid <said@fastlearningcenter.com>
2021-04-17 15:03:33 +0700

    pages and routes

---
+++ saidgatsby/src/pages/about.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+function About() {
+  return (
+    <div>
+      <h2>About page</h2>
+      <h3>Fullstack dev</h3>
+    </div>
+  )
+}
+
+export default About
---
+++ saidgatsby/src/pages/projects/index.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+function Projects() {
+  return (
+    <div>
+      <h2>Projects Page</h2>
+      <p>1</p>
+      <p>2</p>
+      <p>3</p>
+      <p>4</p>
+    </div>
+  )
+}
+
+export default Projects
