6f0653b0ee3fe8bf561733f5f83f106338b6c55c
jejaksaid <said@fastlearningcenter.com>
2021-04-17 16:43:59 +0700

    styling components

--- saidgatsby/src/pages/index.js
+++ saidgatsby/src/pages/index.js
@@ -1,10 +1,11 @@
 import React from "react"
 import Layout from "../components/Layout"
+import { header } from "../styles/home.module.css"
 
 function Home() {
   return (
     <Layout>
-      <section>
+      <section className={header}>
         <h2>Design</h2>
         <h3>Develop & Deploy</h3>
         <p>UX designer & web developer based in Jakarta</p>
