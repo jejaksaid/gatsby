e4fa239ab6a3ac0a56ce5616e33f9a0cad4cf7bc
jejaksaid <said@fastlearningcenter.com>
2021-04-18 14:48:34 +0700

    static file

--- saidgatsby/src/pages/index.js
+++ saidgatsby/src/pages/index.js
@@ -7,12 +7,15 @@ function Home() {
   return (
     <Layout>
       <section className={header}>
-        <h2>Design</h2>
-        <h3>Develop & Deploy</h3>
-        <p>UX designer & web developer based in Jakarta</p>
-        <Link className={btn} to="projects">
-          My Portfolio Projects
-        </Link>
+        <div>
+          <h2>Design</h2>
+          <h3>Develop & Deploy</h3>
+          <p>UX designer & web developer based in Jakarta</p>
+          <Link className={btn} to="projects">
+            My Portfolio Projects
+          </Link>
+        </div>
+        <img src="/banner.png" alt="site banner" style={{ maxWidth: "100%" }} />
       </section>
     </Layout>
   )
