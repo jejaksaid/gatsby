f833eb811dd043e0b37d08f2392495d661751025
jejaksaid <said@fastlearningcenter.com>
2021-04-18 15:08:25 +0700

    siteMetadata & page quaries

--- saidgatsby/gatsby-config.js
+++ saidgatsby/gatsby-config.js
@@ -7,4 +7,9 @@
 module.exports = {
   /* Your site config here */
   plugins: [],
+  siteMetadata: {
+    title: "Said Gatsby",
+    description: "web dev portfolio",
+    copyright: "This website is copyright 2021 said gatsby",
+  },
 }
--- saidgatsby/src/pages/index.js
+++ saidgatsby/src/pages/index.js
@@ -1,9 +1,11 @@
-import { Link } from "gatsby"
+import { graphql, Link } from "gatsby"
 import React from "react"
 import Layout from "../components/Layout"
 import { header, btn } from "../styles/home.module.css"
 
-function Home() {
+function Home({ data }) {
+  console.log(data)
+  const { title, description } = data.site.siteMetadata
   return (
     <Layout>
       <section className={header}>
@@ -16,9 +18,23 @@ function Home() {
           </Link>
         </div>
         <img src="/banner.png" alt="site banner" style={{ maxWidth: "100%" }} />
+        <p>
+          {title} - {description}
+        </p>
       </section>
     </Layout>
   )
 }
 
 export default Home
+
+export const query = graphql`
+  query SiteInfo {
+    site {
+      siteMetadata {
+        title
+        description
+      }
+    }
+  }
+`
