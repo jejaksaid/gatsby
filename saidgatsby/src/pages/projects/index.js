0ff1cf13eec6c1c523a363425d1aaadb85f83ed1
jejaksaid <said@fastlearningcenter.com>
2021-04-18 17:31:59 +0700

    fetching & listing data

--- saidgatsby/src/pages/projects/index.js
+++ saidgatsby/src/pages/projects/index.js
@@ -1,16 +1,46 @@
 import React from "react"
 import Layout from "../../components/Layout"
-import { portfolio } from "../../styles/project.module.css"
+import { portfolio, proyek } from "../../styles/project.module.css"
+import { Link, graphql } from "gatsby"
 
-function Projects() {
+const Projects = ({ data }) => {
+  console.log(data)
+  const projects = data.allMarkdownRemark.nodes
+
   return (
     <Layout>
       <div className={portfolio}>
         <h2>Portfolio</h2>
-        <h3>Projects & webs i've created</h3>
+        <h3>Projects & Websites I've Created</h3>
+        <div className={proyek}>
+          {projects.map(project => (
+            <Link to={"/projects/" + project.frontmatter.slug} key={project.id}>
+              <div>
+                <h3>{project.frontmatter.title}</h3>
+                <p>{project.frontmatter.stack}</p>
+              </div>
+            </Link>
+          ))}
+        </div>
       </div>
     </Layout>
   )
 }
 
 export default Projects
+
+// export page query
+export const query = graphql`
+  query ProjectsPage {
+    allMarkdownRemark {
+      nodes {
+        frontmatter {
+          slug
+          stack
+          title
+        }
+        id
+      }
+    }
+  }
+`
